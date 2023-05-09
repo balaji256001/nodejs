aa925c0d7fc6d6fa22c49366d887201e95c32ae9
balaji256001 <128803229+balaji256001@users.noreply.github.com>
2023-05-09 16:08:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const http = require('http');
 
-console.log('Hello, world');
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, world\n');
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
+
