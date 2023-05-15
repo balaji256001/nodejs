7ec4f49bb9de3fb8c9848c4233cb5b363a9a22fb
balaji256001 <128803229+balaji256001@users.noreply.github.com>
2023-05-15 13:27:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, Balaji\n');
+  res.end('Hello, Bye\n');
 });
 
 server.listen(port, hostname, () => {
